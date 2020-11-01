2853188c75787666c1891bec252bb6416dea6480
CVSink <53992361+CVSink@users.noreply.github.com>
2020-11-01 15:55:24 +0800

    Implement op call

---
+++ nemu/src/cpu/exec/call/call.c
@@ -0,0 +1,10 @@
+#include "cpu/exec/helper.h"
+#include "cpu/decode/modrm.h"
+
+make_helper(call) {
+    uint32_t displacement = instr_fetch(eip + 1, 4);
+    
+    print_asm("call %u", displacement);
+    
+	return 1 + (int)displacement;
+}
